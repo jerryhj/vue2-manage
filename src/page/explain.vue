e5d3ce31533271d54708e76d96c8db64453ac576
jerryhj <jerryhj@yeah.net>
2021-01-03 17:11:01 +0800

    Update explain.vue

--- src/page/explain.vue
+++ src/page/explain.vue
@@ -5,7 +5,7 @@
         <p class="explain_text">跨云数据流动：私有云->公有云，公有云->公有云，公有云->私有云</p>
         <p class="explain_text">增量数据备份：有效减少备份数据容量</p>
         <p class="explain_text">持续数据保护：RPO=0</p>
-        <p class="explain_text">秒级数据恢复：被恢复虚拟机及时可用</p>
+        <p class="explain_text">秒级数据恢复：被恢复虚拟机即时可用</p>
     </div>
 </template>
 v2.1
